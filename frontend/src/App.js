87e80d8bdf86bbed45d387a1177966bbb1a77926
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-08-23 18:33:31 -0300

    first commit (frontend)

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>to-do list.</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
