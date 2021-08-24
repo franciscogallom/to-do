25a60646998d68d1f6a2be36e7c3b03088601d23
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-08-24 12:34:31 -0300

    create connection with MySQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,25 @@
+import { config } from 'dotenv';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
+config();
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      entities: [__dirname + './**/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
   controllers: [],
   providers: [],
 })
