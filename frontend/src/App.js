6d2b44b6d29bad41a272dfc474c1a60d25ad2a0e
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-08-24 23:20:12 -0300

    get tasks with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 import "./App.css"
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    getTasks()
+  }, [])
+
+  const getTasks = () => {
+    axios
+      .get("http://localhost:8080/tasks")
+      .then((res) => {
+        setTasks(res.data)
+        console.log(tasks)
+      })
+      .catch((e) => console.log(e))
+  }
+
   return (
     <div className="App">
-      <header>
-        <h1>to-do list.</h1>
-      </header>
+      <h1>To-Do List</h1>
     </div>
   )
 }
