e3a7716c7886847e085879084dd64450b03ff0f5
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-08-24 23:11:32 -0300

    create ItemsList component

---
+++ frontend/src/components/ItemsList/ItemsList.js
@@ -0,0 +1,25 @@
+import { ListItem } from "../ListItem/ListItem"
+
+import "./itemsList.css"
+
+export function ItemsList({ tasks, setModal, setTaskToEdit, deleteTask }) {
+  return (
+    <div className="items-list">
+      {tasks.length ? (
+        <ul>
+          {tasks.map((item) => (
+            <ListItem
+              key={item.id}
+              item={item}
+              setModal={setModal}
+              setTaskToEdit={setTaskToEdit}
+              deleteTask={deleteTask}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className="no-items">No hay tareas para realizar ✔️.</p>
+      )}
+    </div>
+  )
+}
